5bd170b8b9f9dfc6877930a8e61b3c0835e905c2
Barti <bartosz.majkowski@wp.pl>
2023-01-19 14:20:33 +0100

    express

---
+++ express_mongo/routes/products.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/products', async (req, res) => {
+
+})
+
+router.post('/products', async (req, res) => {
+
+})
+
+router.put('/products/:id', async (req, res) => {
+
+})
+
+router.delete('/products/:id', async (req, res) => {
+
+})
+
+module.exports = router
